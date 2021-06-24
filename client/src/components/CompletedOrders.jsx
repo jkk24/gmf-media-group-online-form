a7ac7f3e5abfaf4744edc6bdca2f3e0e3cfb5f4f
Jeffrey <jkk24@njit.edu>
2021-06-24 04:51:07 -0400

    set up basic dashboard

---
+++ client/src/components/CompletedOrders.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Table, Container } from "react-bootstrap";
+
+const CompletedOrders = () => {
+  return (
+    <Container>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Approve</th>
+            <th>Status</th>
+            <th>Email</th>
+            <th>Name</th>
+            <th>Overall Rating</th>
+            <th>Bedside Manner</th>
+            <th>Wait Time</th>
+            <th>Availability</th>
+            <th>Review</th>
+            <th>Publication Date</th>
+          </tr>
+        </thead>
+      </Table>
+    </Container>
+  );
+};
+
+export default CompletedOrders;
--- client/src/routes/Dashboard.js
+++ client/src/routes/Dashboard.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Tabs, Tab, Container } from "react-bootstrap";
+import PendingOrders from "../components/PendingOrders";
+import CompletedOrders from "../components/CompletedOrders";
 
 const Dashboard = () => {
-  return <div>Hello World!</div>;
+  return (
+    <Container>
+      <h1>Dashboard</h1>
+      <div>
+        <Tabs defaultActiveKey="pendingOrders" id="uncontrolled-tab">
+          <Tab eventKey="pendingOrders" title="Pending Orders">
+            <PendingOrders />
+          </Tab>
+          <Tab eventKey="completedOrders" title="Completed Orders">
+            <CompletedOrders />
+          </Tab>
+        </Tabs>
+      </div>
+    </Container>
+  );
 };
 
 export default Dashboard;
