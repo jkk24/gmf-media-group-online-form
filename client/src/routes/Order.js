af13d89e41bbe21f51287b77bfecf113feccc3fc
Jeffrey <jkk24@njit.edu>
2021-06-28 06:36:11 -0400

    updated order page with more fields

--- client/src/routes/Order.js
+++ client/src/routes/Order.js
@@ -1,7 +1,15 @@
 import React from "react";
 import * as yup from "yup";
 import { Formik } from "formik";
-import { Form, Table, Container, Card, Button } from "react-bootstrap";
+import {
+  Form,
+  Table,
+  Container,
+  Card,
+  Button,
+  Row,
+  Col,
+} from "react-bootstrap";
 import UserAPI from "../apis/UserAPI";
 
 const schema = yup.object().shape({
@@ -17,12 +25,13 @@ const schema = yup.object().shape({
   printingOption10: yup.number().required(),
   printingOption11: yup.number().required(),
   printingOption12: yup.number().required(),
+  printingOptionTotal: yup.number().required(),
 });
 
 UserAPI.defaults.withCredentials = true;
 
 //Lets user input a test object into backend db
-const RegistrationForm = () => {
+const Order = () => {
   return (
     <Formik
       initialValues={{
@@ -56,7 +65,7 @@ const RegistrationForm = () => {
         return (
           <Form onSubmit={handleSubmit}>
             <Container>
-              <Card>
+              <Row className="justify-content-md-center">
                 <Table striped bordered hover>
                   <thead>
                     <tr>
@@ -417,9 +426,297 @@ const RegistrationForm = () => {
                     </tr>
                   </tbody>
                 </Table>
-              </Card>
+              </Row>
+              <Row>
+                <Col>
+                  <Card className="text-center">
+                    <Card.Header>Type of Ad</Card.Header>
+                    <Form.Group>
+                      <Form.Check
+                        name="display"
+                        label="DISPLAY"
+                        onChange={handleChange}
+                        isInvalid={!!errors.display}
+                        feedback={errors.display}
+                        id="display"
+                        feedbackTooltip
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Check
+                        required
+                        name="classified"
+                        label="CLASSIFIED"
+                        onChange={handleChange}
+                        isInvalid={!!errors.classified}
+                        feedback={errors.classified}
+                        id="classified"
+                        feedbackTooltip
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Check
+                        required
+                        name="banner"
+                        label="BANNER"
+                        onChange={handleChange}
+                        isInvalid={!!errors.banner}
+                        feedback={errors.banner}
+                        id="banner"
+                        feedbackTooltip
+                      />
+                    </Form.Group>
+                  </Card>
+                </Col>
+                <Col>
+                  <Card className="text-center">
+                    <Card.Header>DIGITAL SERVICES</Card.Header>
+                    <Container>
+                      <Col>
+                        <Form.Group>
+                          <Form.Check
+                            required
+                            name="webDesign"
+                            label="Web Design"
+                            onChange={handleChange}
+                            isInvalid={!!errors.webDesign}
+                            feedback={errors.webDesign}
+                            id="webDesign"
+                            feedbackTooltip
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Check
+                            required
+                            name="webHosting"
+                            label="Web Hosting"
+                            onChange={handleChange}
+                            isInvalid={!!errors.webHosting}
+                            feedback={errors.webHosting}
+                            id="webHosting"
+                            feedbackTooltip
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Check
+                            required
+                            name="SEO"
+                            label="SEO"
+                            onChange={handleChange}
+                            isInvalid={!!errors.SEO}
+                            feedback={errors.SEO}
+                            id="SEO"
+                            feedbackTooltip
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Check
+                            required
+                            name="localSEO"
+                            label="Local SEO"
+                            onChange={handleChange}
+                            isInvalid={!!errors.localSEO}
+                            feedback={errors.localSEO}
+                            id="localSEO"
+                            feedbackTooltip
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Check
+                            required
+                            name="contentMarketing"
+                            label="Content Marketing"
+                            onChange={handleChange}
+                            isInvalid={!!errors.contentMarketing}
+                            feedback={errors.contentMarketing}
+                            id="contentMarketing"
+                            feedbackTooltip
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Check
+                            required
+                            name="emailMarketing"
+                            label="Email Marketing"
+                            onChange={handleChange}
+                            isInvalid={!!errors.emailMarketing}
+                            feedback={errors.emailMarketing}
+                            id="emailMarketing"
+                            feedbackTooltip
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Check
+                            required
+                            name="emailAutomation"
+                            label="Email Automation"
+                            onChange={handleChange}
+                            isInvalid={!!errors.emailAutomation}
+                            feedback={errors.emailAutomation}
+                            id="emailAutomation"
+                            feedbackTooltip
+                          />
+                        </Form.Group>
+                      </Col>
+                      <Col>
+                        <Form.Group>
+                          <Form.Check
+                            required
+                            name="featuredNews"
+                            label="Featured News"
+                            onChange={handleChange}
+                            isInvalid={!!errors.featuredNews}
+                            feedback={errors.featuredNews}
+                            id="featuredNews"
+                            feedbackTooltip
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Check
+                            required
+                            name="PPC"
+                            label="PPC"
+                            onChange={handleChange}
+                            isInvalid={!!errors.PPC}
+                            feedback={errors.PPC}
+                            id="PPC"
+                            feedbackTooltip
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Check
+                            required
+                            name="adWords"
+                            label="Ad Words"
+                            onChange={handleChange}
+                            isInvalid={!!errors.adWords}
+                            feedback={errors.adWords}
+                            id="adWords"
+                            feedbackTooltip
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Check
+                            required
+                            name="video"
+                            label="Video"
+                            onChange={handleChange}
+                            isInvalid={!!errors.video}
+                            feedback={errors.video}
+                            id="video"
+                            feedbackTooltip
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Check
+                            required
+                            name="linkBuilding"
+                            label="Link Building"
+                            onChange={handleChange}
+                            isInvalid={!!errors.linkBuilding}
+                            feedback={errors.linkBuilding}
+                            id="linkBuilding"
+                            feedbackTooltip
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Check
+                            required
+                            name="adBanners"
+                            label="Ad Banners"
+                            onChange={handleChange}
+                            isInvalid={!!errors.adBanners}
+                            feedback={errors.adBanners}
+                            id="adBanners"
+                            feedbackTooltip
+                          />
+                        </Form.Group>
+                        <Form.Group>
+                          <Form.Check
+                            required
+                            name="branding"
+                            label="Branding"
+                            onChange={handleChange}
+                            isInvalid={!!errors.branding}
+                            feedback={errors.branding}
+                            id="branding"
+                            feedbackTooltip
+                          />
+                        </Form.Group>
+                      </Col>
+                    </Container>
+                  </Card>
+                </Col>
+                <Col>
+                  <Card className="text-center">
+                    <Card.Header>Advertising Duration</Card.Header>
+                    <Form.Group>
+                      <Form.Check
+                        required
+                        name="twoIssues"
+                        label="2x Issues"
+                        onChange={handleChange}
+                        isInvalid={!!errors.twoIssues}
+                        feedback={errors.twoIssues}
+                        id="twoIssues"
+                        feedbackTooltip
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Check
+                        required
+                        name="fourIssues"
+                        label="4x Issues"
+                        onChange={handleChange}
+                        isInvalid={!!errors.fourIssues}
+                        feedback={errors.fourIssues}
+                        id="fourIssues"
+                        feedbackTooltip
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Check
+                        required
+                        name="sixIssues"
+                        label="6x Issues"
+                        onChange={handleChange}
+                        isInvalid={!!errors.sixIssues}
+                        feedback={errors.sixIssues}
+                        id="sixIssues"
+                        feedbackTooltip
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Check
+                        required
+                        name="eightIssues"
+                        label="8x Issues"
+                        onChange={handleChange}
+                        isInvalid={!!errors.eightIssues}
+                        feedback={errors.eightIssues}
+                        id="eightIssues"
+                        feedbackTooltip
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Check
+                        required
+                        name="twelveIssues"
+                        label="12x Issues"
+                        onChange={handleChange}
+                        isInvalid={!!errors.twelveIssues}
+                        feedback={errors.twelveIssues}
+                        id="twelveIssues"
+                        feedbackTooltip
+                      />
+                    </Form.Group>
+                  </Card>
+                </Col>
+              </Row>
+              <Row>
+                <Button type="submit">Submit</Button>
+              </Row>
             </Container>
-            <Button type="submit">Submit</Button>
           </Form>
         );
       }}
@@ -427,4 +724,4 @@ const RegistrationForm = () => {
   );
 };
 
-export default RegistrationForm;
+export default Order;
