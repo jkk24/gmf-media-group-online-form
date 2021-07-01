13fb044d39aa91e439813fbfa89c3c7380f6730c
Jeffrey <jkk24@njit.edu>
2021-06-30 20:40:01 -0400

    some env fixes

---
+++ db/config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config(); // this is important!
+module.exports = {
+  development: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  },
+};
