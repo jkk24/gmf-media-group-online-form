3701a5f3089a33a6e4effaf6afbdd1e486dc9759
Jeffrey <jkk24@njit.edu>
2021-06-30 21:16:31 -0400

    updated sequelize settings for heroku

--- db/models/index.js
+++ db/models/index.js
@@ -9,22 +9,44 @@ const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(
+//     config.database,
+//     config.username,
+//     config.password,
+//     config,
+//     {
+//       dialect: "postgres",
+//       dialectOptions: {
+//         ssl: {
+//           require: true,
+//           rejectUnauthorized: false,
+//         },
+//       },
+//     }
+//   );
+// }
+
+if (process.env.DATABASE_URL) {
+  sequelize = Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 } else {
   sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config,
+    process.env.PGDATABASE,
+    process.env.PGUSER,
+    process.env.PGPASSWORD,
     {
+      host: process.env.PGHOST,
       dialect: "postgres",
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      },
     }
   );
 }
