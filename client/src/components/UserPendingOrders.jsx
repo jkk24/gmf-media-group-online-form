e030c5d1fc2ee9a6f82870be0cea9523c30f90e6
Jeffrey <jkk24@njit.edu>
2021-07-02 13:40:51 -0400

    implemented terms and conditions dialog

--- client/src/components/UserPendingOrders.jsx
+++ client/src/components/UserPendingOrders.jsx
@@ -4,11 +4,34 @@ import OrderAPI from "../apis/OrderAPI";
 import { AuthContext } from "../context/AuthContext";
 import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 
 const UserPendingOrders = () => {
   let history = useHistory();
   const { email } = useContext(AuthContext);
   const [orderList, setOrderList] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [state, setState] = useState({
+    checkedB: false,
+  });
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
 
   const formatDT = (dt) => {
     dt = new Date(dt);
@@ -73,7 +96,7 @@ const UserPendingOrders = () => {
             if (status === "false") {
               element = (
                 <td>
-                  <Button id={order.order_id} onClick={handleApprove}>
+                  <Button id={order.order_id} onClick={handleClickOpen}>
                     Approve
                   </Button>
                 </td>
@@ -82,18 +105,59 @@ const UserPendingOrders = () => {
               element = <td style={{ color: "green" }}>APPROVED</td>;
             }
             return (
-              <tr key={index}>
-                <td>{index}</td>
-                {element}
-                <td>{order.order_id}</td>
-                <td>{formatDT(order.createdAt)}</td>
-                <td>{order.total}</td>
-                <td>
-                  <Link to={"/order/view/" + order.order_id}>View</Link>
-                </td>
-                <td>TODO</td>
-                <td style={{ color: "blue" }}>{order.status}</td>
-              </tr>
+              <>
+                <tr key={index}>
+                  <td>{index}</td>
+                  {element}
+                  <td>{order.order_id}</td>
+                  <td>{formatDT(order.createdAt)}</td>
+                  <td>{order.total}</td>
+                  <td>
+                    <Link to={"/order/view/" + order.order_id}>View</Link>
+                  </td>
+                  <td>TODO</td>
+                  <td style={{ color: "blue" }}>{order.status}</td>
+                </tr>
+                <Dialog
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="form-dialog-title"
+                >
+                  <DialogTitle id="form-dialog-title">
+                    Terms and Conditions
+                  </DialogTitle>
+                  <DialogContent>
+                    <DialogContentText>
+                      Before approving the order, please read our terms and
+                      conditions and check the box when you are ready.
+                    </DialogContentText>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={state.checkedB}
+                          onChange={handleChange}
+                          name="checkedB"
+                          color="primary"
+                        />
+                      }
+                      label="I have read and agree with the Terms and Conditions."
+                    />
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                      Cancel
+                    </Button>
+                    <Button
+                      id={order.order_id}
+                      disabled={!state.checkedB}
+                      onClick={handleApprove}
+                      color="primary"
+                    >
+                      Approve
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+              </>
             );
           })}
         </tbody>
